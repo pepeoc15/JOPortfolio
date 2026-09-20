5d4c0fb12724b43ce2a2181fbced91d2fe2280f8
josunacl <josunacl@uoc.edu>
2026-09-20 17:35:14 +0200

    fix: correct import path for Aura theme and add main CSS file
    Updated the import statement for the Aura theme to the correct package and included the main CSS file for styling.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 
 import 'primeicons/primeicons.css'
+import './styles/main.css'
 
 const app = createApp(App)
 
