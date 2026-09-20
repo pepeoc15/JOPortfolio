bdcb39c740ffad391c4a4cebed8647e380e646a2
josunacl <josunacl@uoc.edu>
2026-09-20 17:10:26 +0200

    feat: add vite-plugin-vue-devtools for enhanced Vue debugging
    Updated package.json and package-lock.json to include vite-plugin-vue-devtools. Modified vite.config.js to integrate the plugin into the Vite configuration for improved development experience.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
